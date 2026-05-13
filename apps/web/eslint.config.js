4424fd2000cbf6756f837de1a2c726a613b61d66
Isabelab-Aguiar <isabelabaguiar.contato@gmail.com>
2026-05-12 23:37:38 -0300

    fix: resolve no-misused-promises errors in auth forms
    allow promise-returning functions in jsx attributes (react-hook-form
    handleSubmit pattern), add eslint-config-next dev dependency

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,15 @@
 const nextConfig = require('@repo/eslint-config/next')
 
 /** @type {import('eslint').Linter.Config[]} */
-module.exports = [{ ignores: ['.next/**', 'node_modules/**'] }, ...nextConfig]
+module.exports = [
+  { ignores: ['.next/**', 'node_modules/**'] },
+  ...nextConfig,
+  {
+    rules: {
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: { attributes: false } },
+      ],
+    },
+  },
+]
